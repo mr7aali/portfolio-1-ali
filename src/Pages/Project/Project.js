28dbd49875ed9a048acd3e0a83d8fa8516409651
mr7aali <mr7aali@gmail.com>
2023-02-24 00:30:40 +0600

    added progress bar

--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -1,30 +1,41 @@
+import { LinearProgress } from '@mui/material';
+import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import './Project.css'
 const Project = () => {
     const [projests, setProjects] = useState(null);
+    const [Loding, setLoading] = useState(true);
     useEffect(() => {
         fetch('https://portfolio-1-server-mr7aali.vercel.app/')
             .then(res => res.json())
-            .then(data => setProjects(data))
+            .then(data => {
+                setProjects(data);
+                setLoading(false)
+            }
+
+            )
     }, [])
 
+
     return (
+
         <div className='m-5'>
             <h1 className='text-4xl text-[#1565C0] text-center font-bold'>Projects</h1>
+            {
+                Loding &&
+                <div className='w-1/4 mx-auto mt-20'>
+                    <LinearProgress />
+                </div>
+            }
             <div className='projects-container'>
-                
-
                 {
                     projests?.map(p => <ProjectCard
                         data={p}
-                        key={p._id} 
+                        key={p._id}
                     ></ProjectCard>)
                 }
-
-
-
             </div>
         </div>
     );
